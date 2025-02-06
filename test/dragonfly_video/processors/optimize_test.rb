cec8b53f680564e992a723848ce2e925e193616f
asgerb <a@asgerbehnckejacobsen.dk>
2025-02-06 13:55:32 +0100

    chore: update test

--- test/dragonfly_video/processors/optimize_test.rb
+++ test/dragonfly_video/processors/optimize_test.rb
@@ -15,6 +15,53 @@ describe DragonflyVideo::Processors::Optimize do
     _(movie.size).must_be :<, original_size
   end
 
+  describe "profiles" do
+    it "accepts streaming profile" do
+      processor.call(video, profile: :streaming)
+      movie = FFMPEG::Movie.new(video.path)
+
+      _(movie).must_be :valid?
+      _(movie.video_codec).must_equal "h264"
+      _(movie.video_stream).must_include "(Main)"
+    end
+
+    it "accepts archive profile" do
+      processor.call(video, profile: :archive)
+      movie = FFMPEG::Movie.new(video.path)
+
+      _(movie).must_be :valid?
+      _(movie.video_codec).must_equal "h264"
+      _(movie.video_stream).must_include "(High)"
+    end
+
+    it "accepts mobile profile" do
+      processor.call(video, profile: :mobile)
+      movie = FFMPEG::Movie.new(video.path)
+
+      _(movie).must_be :valid?
+      _(movie.video_codec).must_equal "h264"
+      _(movie.video_stream).must_include "(Constrained Baseline)"
+    end
+
+    it "uses default profile when none specified" do
+      processor.call(video)
+      movie = FFMPEG::Movie.new(video.path)
+
+      _(movie).must_be :valid?
+      _(movie.video_codec).must_equal "h264"
+      _(movie.video_stream).must_include "(Main)"
+    end
+
+    it "uses default profile for invalid profiles" do
+      processor.call(video, profile: :invalid_profile)
+      movie = FFMPEG::Movie.new(video.path)
+
+      _(movie).must_be :valid?
+      _(movie.video_codec).must_equal "h264"
+      _(movie.video_stream).must_include "(Main)"
+    end
+  end
+
   it 'does not change the video extension' do
     processor.call(video)
 
